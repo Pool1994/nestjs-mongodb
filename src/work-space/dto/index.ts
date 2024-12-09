f23e07a0700717ac14ba5231f7fb60db76364fdb
Pool <pool.vera@alarcongroup.us>
2024-12-09 10:22:05 -0800

    Adds DTOs for lead workspace, participants
    Adds `LeadWorkspaceDto`, `ParticipantsDto` to the DTOs export.

--- src/work-space/dto/index.ts
+++ src/work-space/dto/index.ts
@@ -1,4 +1,6 @@
 export * from './appointment.dto';
+export * from './lead-worspace.dto';
 export * from './meeting.dto';
+export * from './participants.dto';
 export * from './task.dto';
 export * from './workspace.dto';
